05f1ba1197ef9acc9aa8ff41c356ad118f083691
balgard <student@balgard.local>
2017-04-11 12:34:27 -0500

    Fixed Card class.

--- Card Koenig/ViewController.swift
+++ Card Koenig/ViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var card  = Card()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
