374c983c20737c68e0e58efeafb01900b7d7263a
balgard <student@balgard.local>
2017-04-12 11:27:07 -0500

    Added President Class, created new viewController, need to get it to work.

---
+++ Card Koenig/PresidentViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PresidentViewController.swift
+//  Card Koenig
+//
+//  Created by Balgard on 4/12/17.
+//  Copyright © 2017 Balgard. All rights reserved.
+//
+
+import UIKit
+
+class PresidentViewController: UIView
+{
+
+    func viewDidLoad()
+    {
+        
+    }
+
+}
