2159f308456db207dec96ed1b5ac5d0c43008e96
Tconti <student@tconti.local>
2017-04-13 11:16:01 -0500

    Added War class

---
+++ Card Koenig/War.swift
@@ -0,0 +1,21 @@
+//
+//  War.swift
+//  Card Koenig
+//
+//  Created by Tconti on 4/13/17.
+//  Copyright © 2017 Balgard. All rights reserved.
+//
+
+import UIKit
+
+class War: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
