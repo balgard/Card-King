373e73c301f1d24b6e631362e44581343466df88
Brent Behling <bbehling@brents-macbook-air-2.local>
2017-05-09 11:14:20 -0500

    Added go fish card checks

--- Card Koenig/GoFishView.swift
+++ Card Koenig/GoFishView.swift
@@ -2,13 +2,39 @@
 //  GoFishView.swift
 //  Card Koenig
 //
-//  Created by Brent Behling on 4/18/17.
+//  Created by Brent Behling on 5/8/17.
 //  Copyright © 2017 Balgard. All rights reserved.
 //
 
 import UIKit
 
-class GoFishView : UIView {
+class GoFishView: UIView {
+    
+    var userCards = [Card]()
+    var cpuCards = [Card]()
+    var index = 0
+    
     func viewDidLoad()  {
+        
+    }
+    
+    func userCheckDeck(selectedCard: Card)    {
+        for card in cpuCards    {
+            if(selectedCard.value == card.value)    {
+                print("Is there")
+                userCards.append(card)
+                cpuCards.remove(at: cpuCards.index(of: card)!)
+            }
+        }
+    }
+    
+    func cpuCheckDeck(selectedCard: Card)    {
+        for card in userCards    {
+            if(selectedCard.value == card.value)    {
+                print("Is there")
+                cpuCards.append(card)
+                userCards.remove(at: userCards.index(of: card)!)
+            }
+        }
     }
 }
