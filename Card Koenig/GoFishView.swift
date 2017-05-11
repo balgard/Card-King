2901c29405a0f61cbc4a08e91366bfd035219b31
Brent Behling <bbehling@brents-macbook-air-2.local>
2017-05-11 08:16:41 -0500

    added an array of completed pairs

--- Card Koenig/GoFishView.swift
+++ Card Koenig/GoFishView.swift
@@ -16,30 +16,75 @@ import UIKit
 
 class GoFishView: UIView {
     
-    var userCards = [Card]()
-    var cpuCards = [Card]()
+    var userOneCards = [Card]()
+    var userOnePairs = [Card]()
+    var userTwoCards = [Card]()
+    var userTwoPairs = [Card]()
+    var userTurn = true
     var index = 0
+    var i = 0
+    var k = 0
+    var x = 0
     
     func viewDidLoad()  {
         
     }
     
-    func userCheckDeck(selectedCard: Card)    {
-        for card in cpuCards    {
+    func checkForUserOnePairs()  {
+        i = userOneCards.count
+        k = 0
+        x = 0
+        while(k < i)    {
+            x = k + 1
+            while(x < i)    {
+                if(userOneCards[k].value == userOneCards[x].value)  {
+                    userOnePairs.append(userOneCards[k])
+                    userOneCards.remove(at: x)
+                    userOneCards.remove(at: k)
+                }
+            }
+            k += 1
+        }
+    }
+    
+    func checkForUserTwoPairs()  {
+        i = userTwoCards.count
+        k = 0
+        x = 0
+        while(k < i)    {
+            x = k + 1
+            while(x < i)    {
+                if(userTwoCards[k].value == userTwoCards[x].value)  {
+                    userTwoPairs.append(userTwoCards[k])
+                    userTwoCards.remove(at: x)
+                    userTwoCards.remove(at: k)
+                    break
+                }
+            }
+            k += 1
+        }
+    }
+    
+    func userOneCheckDeck(selectedCard: Card)    {
+        for card in userTwoCards    {
             if(selectedCard.value == card.value)    {
-                print("Is there")
-                userCards.append(card)
-                cpuCards.remove(at: cpuCards.index(of: card)!)
+                print("Is in cpu")
+                userOneCards.append(card)
+                userTwoCards.remove(at: userTwoCards.index(of: card)!)
+                checkForUserOnePairs()
+                break
             }
         }
     }
     
-    func cpuCheckDeck(selectedCard: Card)    {
-        for card in userCards    {
+    func userTwoCheckDeck(selectedCard: Card)    {
+        for card in userOneCards    {
             if(selectedCard.value == card.value)    {
-                print("Is there")
-                cpuCards.append(card)
-                userCards.remove(at: userCards.index(of: card)!)
+                print("Is in user")
+                userTwoCards.append(card)
+                userOneCards.remove(at: userOneCards.index(of: card)!)
+                checkForUserTwoPairs()
+                break
             }
         }
     }
