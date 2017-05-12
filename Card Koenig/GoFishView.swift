040fa8ecbdde42640a4af88bdb918e2046f9b16a
Brent Behling <bbehling@brents-macbook-air-2.local>
2017-05-11 22:22:06 -0500

    Shuffle and game beginning stuff

--- Card Koenig/GoFishView.swift
+++ Card Koenig/GoFishView.swift
@@ -16,6 +16,7 @@ import UIKit
 
 class GoFishView: UIView {
     
+    var standardDeck = [Card]()
     var userOneCards = [Card]()
     var userOnePairs = [Card]()
     var userTwoCards = [Card]()
@@ -23,6 +24,8 @@ class GoFishView: UIView {
     var drawCards = [Card]()
     var userTurn = true
     var index = 0
+    
+    //For checkForUserPairs
     var i = 0
     var k = 0
     var x = 0
@@ -69,6 +72,7 @@ class GoFishView: UIView {
                 userOneCards.remove(at: c2Pos)
                 userOneCards.remove(at: c1Pos)
                 i = userOneCards.count
+                organizeCardPair()
             }   else    {
                 k += 1
             }
@@ -76,21 +80,6 @@ class GoFishView: UIView {
     }
     
     func checkForUserTwoPairs()  {
-        /*i = userTwoCards.count
-        k = 0
-        x = 0
-        while(k < i)    {
-            x = k + 1
-            while(x < i)    {
-                if(userTwoCards[k].value == userTwoCards[x].value)  {
-                    userTwoPairs.append(userTwoCards[k])
-                    userTwoCards.remove(at: x)
-                    userTwoCards.remove(at: k)
-                    break
-                }
-            }
-            k += 1
-        }*/
         i = userTwoCards.count
         k = 0
         x = 0
@@ -122,6 +111,7 @@ class GoFishView: UIView {
                 userTwoCards.remove(at: c2Pos)
                 userTwoCards.remove(at: c1Pos)
                 i = userTwoCards.count
+                organizeCardPair()
             }   else    {
                 k += 1
             }
@@ -186,16 +176,119 @@ class GoFishView: UIView {
         }
     }
     
-    func goFishDeal(/*player: Int, handSize: Int, */deckSize: Int)    {
+    func goFishDeal(deckSize: Int)    {
         while(deckSize != 0)    {
             if(userOneCards.count < 7) {
-                userOneCards.append()
-                //Remove card from the deck
+                userOneCards.append(standardDeck[0])
+                standardDeck.remove(at: 0)
             }   else if(userTwoCards.count < 7)    {
-                userTwoCards.append()
+                userTwoCards.append(standardDeck[0])
+                standardDeck.remove(at: 0)
             }   else    {
-                drawCards.append()
+                drawCards.append(standardDeck[0])
+                standardDeck.remove(at: 0)
             }
         }
+    }
+    
+    func shuffle(deck: [AnyObject])
+    {
+        var tempDeck = [Card]()
+        while standardDeck.count > 0
+        {
+            var random = Int(arc4random_uniform(UInt32(standardDeck.count)))
+            var card = standardDeck.remove(at: random)
+            tempDeck.append(card)
+        }
+        standardDeck = tempDeck
+    }
+    
+    func fillDeck(deck: [AnyObject])
+    {
+        var i = 0
+        var x = 1
+        var temp = deck
+        while i < 4
+        {
+            var letter = ""
+            var suite = ""
+            var color = ""
+            if i == 0
+            {
+                letter = "c"
+                suite = "Clubs"
+                color = "Black"
+            }
+            else if i == 1
+            {
+                letter = "d"
+                suite = "Diamonds"
+                color = "Red"
+            }
+            else if i == 2
+            {
+                letter = "h"
+                suite = "Hearts"
+                color = "Red"
+            }
+            else
+            {
+                letter = "s"
+                suite = "Spades"
+                color = "Black"
+            }
+            while x < 14
+            {
+                var imageName = ""
+                if x < 10
+                {
+                    imageName = "\(letter)0\(x)"
+                }
+                else
+                {
+                    imageName = "\(letter)\(x)"
+                }
+                var c = Card()
+                c.value = x
+                c.suite = suite
+                c.color = color
+                c.imageName = imageName
+                c.assignRank(value: c.value)
+                c.setImage(name: c.imageName)
+                temp.append(c)
+                x += 1
+            }
+            x = 1
+            i += 1
+        }
+        standardDeck = temp as! [Card]
+    }
+    
+    func organizeCardDeck() {
+        userOneCards = userOneCards.sorted { $0.value < $1.value }
+        userTwoCards = userTwoCards.sorted { $0.value < $1.value }
+        drawCards = drawCards.sorted { $0.value < $1.value }
+    }
+    
+    func organizeCardPair() {
+        userOnePairs = userOnePairs.sorted { $0.value < $1.value }
+        userTwoPairs = userTwoPairs.sorted { $0.value < $1.value }
+    }
+
+    func gameStart()    {
+        standardDeck.removeAll()
+        userOneCards.removeAll()
+        userOnePairs.removeAll()
+        userTwoCards.removeAll()
+        userTwoPairs.removeAll()
+        drawCards = [Card]()
+        userTurn = true
+        
+        fillDeck(deck: standardDeck)
+        shuffle(deck: standardDeck)
+        goFishDeal(deckSize: standardDeck.count)
+        organizeCardDeck()
+        checkForUserOnePairs()
+        checkForUserTwoPairs()
     }
 }
